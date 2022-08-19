a70dbe88cf134348f7bc2c9011b44045ee76d7f0
riappstudiotester <107489117+riappstudiotester@users.noreply.github.com>
2022-08-19 16:50:45 +0100

    Angular part

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav', //use this to add this to another  component
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
