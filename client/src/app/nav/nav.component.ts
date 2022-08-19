cd7df8c646cee102ae556a597ecb0e971f890c59
riappstudiotester <107489117+riappstudiotester@users.noreply.github.com>
2022-08-19 17:04:39 +0100

    Still working well here

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
   constructor() {}
 
   ngOnInit(): void {}
+
+  login() {
+    console.log(this.model);
+  }
 }
